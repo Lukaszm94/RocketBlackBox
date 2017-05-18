e7ecf67a21262f61d9b03c81d8e5569697ffa65a
Lukaszm94 <Lukaszm6@gmail.com>
2017-05-18 17:41:21 +0200

    Add device logic code, code is flight ready <hopefully>

--- src/main.c
+++ src/main.c
@@ -13,6 +13,10 @@
 #include "millis.h"
 #include "uart.h"
 #include "ringbuffer.h"
+#include "memory.h"
+#include "memorymanager.h"
+#include "buttons.h"
+#include "config.h"
 			
 #define LED_PIN GPIO_Pin_6
 
@@ -56,36 +60,77 @@ void USART1_IRQHandler()
 	}
 }
 
+void perform_startup_sequence()
+{
+	delay(200);
+	if(!buttons_is_button_pressed(BUTTON_1)) {
+		return;
+	}
+	delay(2000);
+	if(buttons_is_button_pressed(BUTTON_1)) {
+		return;
+	}
+	uint32_t wait_until = millis() + 3000;
+	uint8_t button_was_pressed = 0;
+#if DEBUG
+	UART_sendString("Waiting 3000ms for button press\n");
+#endif
+	while(millis() < wait_until) {
+		if(buttons_is_button_pressed(BUTTON_1)) {
+			button_was_pressed = 1;
+			break;
+		}
+	}
+	if(!button_was_pressed) {
+		return;
+	}
+	mm_dump_memory();
+	// 10 seconds window to send erase command
+	wait_until = millis() + 10000;
+	uint8_t erase = 0;
+#if DEBUG
+	UART_sendString("Waiting 10000ms for erase command\n");
+#endif
+	// empty input buffer
+	char c;
+	while(ring_buffer_num_items(&usart_rx_buffer)) {
+		ring_buffer_dequeue(&usart_rx_buffer, &c);
+	}
+	while(millis() < wait_until) {
+		if(ring_buffer_num_items(&usart_rx_buffer)) {
+			ring_buffer_dequeue(&usart_rx_buffer, &c);
+			if(c == 'e') {
+				erase = 1;
+			}
+			break;
+		}
+	}
+	if(erase) {
+		mm_reset_memory();
+	}
+}
+
 int main(void)
 {
 	RCC_conf();
 	millis_init();
-	initGPIO();
-
-	ring_buffer_init(&usart_rx_buffer);
 	UART_init();
-
-
+	memory_init();
+	memory_unlock_protection();
+	buttons_init();
+	ring_buffer_init(&usart_rx_buffer);
+	mm_init();
+	perform_startup_sequence();
 
 	for(;;) {
-		if(ring_buffer_num_items(&usart_rx_buffer) > (RING_BUFFER_SIZE - 10)) {
+		if(ring_buffer_num_items(&usart_rx_buffer) >= (RING_BUFFER_SIZE - 1)) {
 			NVIC_DisableIRQ(USART1_IRQn);
 			char buff[RING_BUFFER_SIZE];
 			uint8_t bytes = ring_buffer_num_items(&usart_rx_buffer);
 			uint8_t bytesRead = ring_buffer_dequeue_arr(&usart_rx_buffer, &buff[0], bytes);
-			if(bytesRead != bytes) {
-				GPIO_SetBits(GPIOA, LED_PIN);
-			}
 			NVIC_EnableIRQ(USART1_IRQn);
-			for(int i = 0; i < bytesRead; i++) {
-				UART_sendChar(buff[i]);
-			}
-
-			//UART_sendChar('\n');
+			mm_append_page((uint8_t*)buff, bytesRead);
 		}
-//		NVIC_DisableIRQ(USART1_IRQn);
-//		delay(500);
-//		NVIC_EnableIRQ(USART1_IRQn);
-//		delay(10);
+		mm_update_autosave();
 	}
 }
